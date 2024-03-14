57c653d6559be4cf716eb33ef24966a07a19d8c6
nagudu <halifashuaibu12@gmail.com>
2024-03-14 10:49:39 +0100

    save

--- src/controllers/super_agent.js
+++ src/controllers/super_agent.js
@@ -1,19 +1,19 @@
 // const bcrypt = require ('bcryptjs';)
-const db = require("../models");
+const db = require('../models');
+const User = db.User;
+
 
 module.exports.superAgent = async (req, res) => {
   const {
     query_type = "insert",
-    id = null,
-    name = null,
-    phone = null,
-    email = null,
-    address = null,
-    vendor = null,
-    state = null,
-    lga = null,
-    nin = null,
-    dob = null
+    id = "",
+    name,
+    phone,
+    email,
+    address,
+    vendor,
+    state,
+    lga,
   } = req.body;
 
   try {
@@ -21,14 +21,12 @@ module.exports.superAgent = async (req, res) => {
       `CALL super_agent(:query_type, 
         :id, 
         :name,
-        :phone,
-        :email,
-        :address,
-        :vendor,
-        :state,
-        :lga,
-        :nin,
-        :dob)`,
+            :phone,
+            :email,
+            :address,
+            :vendor,
+            :state,
+            :lga)`,
       {
         replacements: {
           query_type,
@@ -40,8 +38,6 @@ module.exports.superAgent = async (req, res) => {
           vendor,
           state,
           lga,
-          nin,
-          dob
         },
       }
     );
@@ -55,62 +51,52 @@ module.exports.superAgent = async (req, res) => {
   }
 };
 
-//  @ Get all superagent
-//  @route GET /superagent
-module.exports.getAllSuperAgent = async (req, res) => {
-  db.sequelize
-    .query(`select * from super_agent`)
-    .then((resp) => res.status(500).json({ success: true, resp }))
-    .catch((err) =>
-      res
-        .status(200)
-        .json({ success: false, error: "failed to fetch super agent" })
-    );
-};
 //   @ Fetch a single super agent by ID
 //   @route GET /api/superagent/:id
-module.exports.fetchSingleSuperAgent = async (req, res) => {
-  const { id } = req.params;
-  if (!ObjectId.isValid(id)) return res.status(404).send("Invalid Id");
-
-  db.super_agent
-    .findOne({ where: { id: ObjectId(id) } })
-    .then((result) => {
-      if (!result) return res.status(404).send("No user with that ID");
-      res.status(200) 
-        .json({ success: true, data: result });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ success: false, error: err });
-    });
-};
-
-//   @ Update Super Agent Information
-//   @route PUT /api/superagent/:id
-module.exports.updateSuperAgentInfo = async (req, res) => { 
-  const { id } = req.params;
-  const updates = req.body;
-
-  // check validation
-  const validateUpdate = await superagentValidation.validateUpdate(updates);
-  if (!validateUpdate.valid)
-    return res.status(400).send(validateUpdate.error.message);
-
-  // find and update the record in database
-  db.super_agent.update(updates, { where: { id: ObjectId(id) } })
-    .then((updatedUser) => {
-      if (!updatedUser[0]) return res.status(400).send("No User Found!");
-      res.status(200).json({ 
-        success: true, 
-        count: updatedUser[0] ?.count,  
-        data: updatedUser[0]?.dataValues  
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({  
-        success: false,  
-        error: err.toString()   
-      });
-    });
-};
+module.exports.fetchSuperAgent = async (req, res) => {
+    const {
+      query_type = "select",
+      id = null,
+      name = null,
+      phone=null,
+      email=null,
+      address=null,
+      vendor=null,
+      state=null,
+      lga=null,
+    } = req.query;
+  
+    try {
+      const resp = await db.sequelize.query(
+        `CALL super_agent(:query_type, 
+          :id, 
+          :name,
+              :phone,
+              :email,
+              :address,
+              :vendor,
+              :state,
+              :lga)`,
+        {
+          replacements: {
+            query_type,
+            id,
+            name,
+            phone,
+            email,
+            address,
+            vendor,
+            state,
+            lga,
+          },
+        }
+      );
+  
+      res.status(200).json({ success: true, results: resp });
+    } catch (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ success: false, error: "Failed to register super agent" });
+    }
+  }
--- src/routes/super_agent.js
+++ src/routes/super_agent.js
@@ -1,5 +1,5 @@
 
-const { superAgent, getAllSuperAgent } = require("../controllers/super_agent");
+const { superAgent, fetchSuperAgent } = require("../controllers/super_agent");
 
 module.exports = (app) => {
   // create a new superagent
@@ -8,50 +8,10 @@ module.exports = (app) => {
     superAgent
   );
 
-  // select all superagent from the database.
-  app.get('/getallsuperagent/get',
-    getAllSuperAgent);
-    /* 
-      .then((results)=>{
-        console.log('API RESULTS: ', results);
-      }) */
-  
-  // update an agent's information in the database 
-  app.put("/api/update/:id", function(req, res){
-    let id = req.params.id;
-    let data = req.body;
-    
-    superAgent.findByIdAndUpdate(id, data).then(()=>{
-      res.sendStatus(200);
-    }).catch((err)=>console.error(err));
-  });
-
-  // delete an agent from the database
-  app.delete("/api/remove/:id", function(req,res){ 
-    let id=req.params.id ;
-    superAgent.findByIdAndRemove(id).then(()=>{
-      res.json({ removed: 'true'});
-    }).catch((err)=>{
-      console.error(err)
-      res.status(500).json({ error: "An error occurred while deleting the super agent" });
-    })
-  });
+    app.get('/superagent'
+    ,fetchSuperAgent);
 
 
-
-    // update an existing super agent by id
-  app.put("/api/:id", function(req, res) {
-  const id = req.params.id;
-  const updatedData = req.body;
-  superAgent.findOneAndUpdate({ _id: id }, updatedData, { new: true })
-    .then(updatedSuperAgent => {
-      res.json(updatedSuperAgent);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: "An error occurred while updating the super agent" });
-    });
-}); 
 
 }
 
