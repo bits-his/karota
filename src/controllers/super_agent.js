7d0b45a22ffaf4231b2d0a50cd85d5b56a197b62
ibagwa9 <ibagwai9@gmail.com>
2024-03-14 15:24:36 +0100

    cx

--- src/controllers/super_agent.js
+++ src/controllers/super_agent.js
@@ -6,14 +6,16 @@ const User = db.User;
 module.exports.superAgent = async (req, res) => {
   const {
     query_type = "insert",
-    id = "",
-    name,
-    phone,
-    email,
-    address,
-    vendor,
-    state,
-    lga,
+    id = null,
+    name = null,
+    phone = null,
+    email = null,
+    address = null,
+    vendor = null,
+    state = null,
+    lga = null,
+    nin = null,
+    dob = null,
   } = req.body;
 
   try {
@@ -26,7 +28,9 @@ module.exports.superAgent = async (req, res) => {
             :address,
             :vendor,
             :state,
-            :lga)`,
+            :lga,
+            :nin,
+            :dob)`,
       {
         replacements: {
           query_type,
@@ -38,6 +42,8 @@ module.exports.superAgent = async (req, res) => {
           vendor,
           state,
           lga,
+          nin,
+          dob
         },
       }
     );
@@ -54,49 +60,56 @@ module.exports.superAgent = async (req, res) => {
 //   @ Fetch a single super agent by ID
 //   @route GET /api/superagent/:id
 module.exports.fetchSuperAgent = async (req, res) => {
-    const {
-      query_type = "select",
-      id = null,
-      name = null,
-      phone=null,
-      email=null,
-      address=null,
-      vendor=null,
-      state=null,
-      lga=null,
-    } = req.query;
-  
-    try {
-      const resp = await db.sequelize.query(
-        `CALL super_agent(:query_type, 
-          :id, 
-          :name,
-              :phone,
-              :email,
-              :address,
-              :vendor,
-              :state,
-              :lga)`,
-        {
-          replacements: {
-            query_type,
-            id,
-            name,
-            phone,
-            email,
-            address,
-            vendor,
-            state,
-            lga,
-          },
-        }
-      );
-  
-      res.status(200).json({ success: true, results: resp });
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ success: false, error: "Failed to register super agent" });
-    }
+  const {
+    query_type = "insert",
+    id = null,
+    name = null,
+    phone = null,
+    email = null,
+    address = null,
+    vendor = null,
+    state = null,
+    lga = null,
+    nin = null,
+    dob = null,
+  } = req.query;
+
+  try {
+    const resp = await db.sequelize.query(
+      `CALL super_agent(:query_type, 
+        :id, 
+        :name,
+            :phone,
+            :email,
+            :address,
+            :vendor,
+            :state,
+            :lga,
+            :nin,
+            :dob)`,
+      {
+        replacements: {
+          query_type,
+          id,
+          name,
+          phone,
+          email,
+          address,
+          vendor,
+          state,
+          lga,
+          nin,
+          dob
+        },
+      }
+    );
+
+
+    res.status(200).json({ success: true, results: resp });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, error: "Failed to register super agent" });
   }
+}
