a693f9b54902d3d977fabafec52f050db9996da3
nagudu <halifashuaibu12@gmail.com>
2024-03-14 09:18:41 +0100

    save

--- src/controllers/super_agent.js
+++ src/controllers/super_agent.js
@@ -61,3 +61,50 @@ module.exports.getAllSuperAgent = async (req, res) => {
         .json({ success: false, error: "failed to fetch super agent" })
     );
 };
+//   @ Fetch a single super agent by ID
+//   @route GET /api/superagent/:id
+module.exports.fetchSingleSuperAgent = async (req, res) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) return res.status(404).send("Invalid Id");
+
+  db.super_agent
+    .findOne({ where: { id: ObjectId(id) } })
+    .then((result) => {
+      if (!result) return res.status(404).send("No user with that ID");
+      res.status(200) 
+        .json({ success: true, data: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ success: false, error: err });
+    });
+};
+
+//   @ Update Super Agent Information
+//   @route PUT /api/superagent/:id
+module.exports.updateSuperAgentInfo = async (req, res) => { 
+  const { id } = req.params;
+  const updates = req.body;
+
+  // check validation
+  const validateUpdate = await superagentValidation.validateUpdate(updates);
+  if (!validateUpdate.valid)
+    return res.status(400).send(validateUpdate.error.message);
+
+  // find and update the record in database
+  db.super_agent.update(updates, { where: { id: ObjectId(id) } })
+    .then((updatedUser) => {
+      if (!updatedUser[0]) return res.status(400).send("No User Found!");
+      res.status(200).json({ 
+        success: true, 
+        count: updatedUser[0] ?.count,  
+        data: updatedUser[0]?.dataValues  
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({  
+        success: false,  
+        error: err.toString()   
+      });
+    });
+};
--- src/routes/super_agent.js
+++ src/routes/super_agent.js
@@ -7,11 +7,53 @@ module.exports = (app) => {
     '/api/superagent/create',
     superAgent
   );
+
   // select all superagent from the database.
   app.get('/api/getallsuperagent/get',
     getAllSuperAgent);
+    /* 
+      .then((results)=>{
+        console.log('API RESULTS: ', results);
+      }) */
+  
+  // update an agent's information in the database 
+  app.put("/api/update/:id", function(req, res){
+    let id = req.params.id;
+    let data = req.body;
+    
+    superAgent.findByIdAndUpdate(id, data).then(()=>{
+      res.sendStatus(200);
+    }).catch((err)=>console.error(err));
+  });
+
+  // delete an agent from the database
+  app.delete("/api/remove/:id", function(req,res){ 
+    let id=req.params.id ;
+    superAgent.findByIdAndRemove(id).then(()=>{
+      res.json({ removed: 'true'});
+    }).catch((err)=>{
+      console.error(err)
+      res.status(500).json({ error: "An error occurred while deleting the super agent" });
+    })
+  });
 
 
 
+    // update an existing super agent by id
+  app.put("/api/:id", function(req, res) {
+  const id = req.params.id;
+  const updatedData = req.body;
+  superAgent.findOneAndUpdate({ _id: id }, updatedData, { new: true })
+    .then(updatedSuperAgent => {
+      res.json(updatedSuperAgent);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred while updating the super agent" });
+    });
+}); 
 
-};
+}
+
+
+
